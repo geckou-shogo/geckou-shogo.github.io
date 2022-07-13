3a8d6303f367b5ff050a4ead4726d322fdd33712
geckou-irei <taishi.irei@geckou.net>
2022-07-13 11:02:17 +0900

    横スクロール途中

--- pages/index.vue
+++ pages/index.vue
@@ -4,32 +4,32 @@
       <div id="scroll_wrap" :class="$style.scroll_wrap">
         <section id="vision" :class="$style.vision" class="scroll_item">
           <div :class="$style.container">
-              <h2 :class="$style.subttl">1</h2>
+            <h2 :class="$style.subttl">1</h2>
           </div>
-        </section> 
+        </section>
         <section id="service" :class="$style.service" class="scroll_item">
           <div :class="$style.container">
-              <h2 :class="$style.subttl">2</h2>
+            <h2 :class="$style.subttl">2</h2>
           </div>
         </section>
         <section id="company" :class="$style.company" class="scroll_item">
           <div :class="$style.container">
-              <h2 :class="$style.subttl">3</h2>
+            <h2 :class="$style.subttl">3</h2>
           </div>
         </section>
         <section id="contact" :class="$style.contact" class="scroll_item">
           <div :class="$style.container">
-              <h2 :class="$style.subttl">4</h2>
+            <h2 :class="$style.subttl">4</h2>
           </div>
         </section>
-        </div>
       </div>
+    </div>
   </main>
 </template>
 
 <script lang="ts">
-import gsap from 'gsap';
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 export default {
   data() {
@@ -42,46 +42,48 @@ export default {
   methods: {
     calculateWindowWidth() {
       if (window.innerWidth > window.innerHeight) {
-        console.log('横幅が大きい'); 
-        const area  = document.querySelector('#scroll');
-        const wrap  = document.querySelector('#scroll_wrap');
-        const items = document.querySelectorAll('.scroll_item');
-        const num   = items.length;
+        console.log('横幅が大きい')
+        const area = document.querySelector('#scroll')
+        const wrap = document.querySelector('#scroll_wrap')
+        const items = document.querySelectorAll('.scroll_item')
+        const num = items.length
 
-        gsap.set(wrap,  { width: num * 100 + "%" });
-        gsap.set(items, { width: 100 / num + "%" });
+        gsap.set(wrap, { width: num * 100 + '%' })
+        gsap.set(items, { width: 100 / num + '%' })
 
         gsap.to(items, {
-          xPercent: -100 * ( num - 1 ), //x方向に移動させる
-          ease: "none",
+          xPercent: -100 * (num - 1), //x方向に移動させる
+          ease: 'none',
           scrollTrigger: {
             trigger: area,
-            start: "top top",
-            end: "+=100%",
+            start: 'top top',
+            end: '+=100%',
             scrub: 1, //スクロール量に応じて動かす
             pin: true, //ピン留め
-            snap: { //キリの良い位置へ移動させる
-              snapTo: 1 / ( num - 1 ),
+            snap: {
+              //キリの良い位置へ移動させる
+              snapTo: 1 / (num - 1),
               duration: 0.5,
             },
           },
-        });
-      } 
-      console.log('width=' + innerWidth);
-      console.log('height=' + innerHeight);
+        })
+      }
+      console.log('width=' + innerWidth)
+      console.log('height=' + innerHeight)
     },
     handleScroll() {
-        this.scrollY = window.scrollY;
-        console.log(handleScroll);
-        
-    }
+      this.scrollY = window.scrollY
+      console.log(this.scrollY)
+    },
   },
   mounted() {
-      window.addEventListener('resize', this.calculateWindowWidth, 'scroll', this.handleScroll)
-      gsap.registerPlugin(ScrollTrigger);
-      this.$nextTick(() => {
-      this.calculateWindowWidth();
-    });
+    window.addEventListener('resize', this.calculateWindowWidth)
+    window.addEventListener('scroll', this.handleScroll)
+    gsap.registerPlugin(ScrollTrigger)
+    this.$nextTick(() => {
+      this.calculateWindowWidth()
+      this.handleScroll()
+    })
   },
 }
 </script>
@@ -91,7 +93,6 @@ export default {
 @use '~/assets/scss/font' as f;
 @use '~/assets/scss/color' as c;
 
-
 .scroll {
   overflow: hidden;
   &_wrap {
@@ -100,14 +101,12 @@ export default {
       display: block;
     }
   }
-  
 }
 
 .subttl {
   font-size: 100px;
   color: c.$black;
 }
-
 
 .container {
   display: flex;
@@ -134,5 +133,4 @@ export default {
 .contact {
   background-color: red;
 }
-
 </style>
