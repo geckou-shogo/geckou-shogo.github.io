5f6f74691572ef0e98dae5f8356b1d685ef1fbe8
geckou-shogo <shogo.nojima@geckou.net>
2022-07-21 17:47:18 +0900

    ソースを綺麗に

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,19 @@
 <template>
-<main id="frontpage">
-  <div :class="$style.scroll" class="scroll">
-    <section 
-      class="scroll_item"
-      v-for="item in sectionData"
-      :class="$style.section"
-      :key="item.id"
-      :data-color="item.color"
-    >
-      <CommonContainer
-        :sectionData="item"
+  <main id="frontpage">
+    <div :class="$style.scroll" class="scroll">
+      <section 
+        v-for="item in sectionData"
+        :class="[$style.section, 'scroll_item']"
+        :key="item.id"
+        :data-color="item.color"
       >
-      </CommonContainer>
-    </section>
-</div>
-
-<div class="center-marker"></div>
-</main>
+        <CommonContainer
+          :sectionData="item"
+        >
+        </CommonContainer>
+      </section>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -53,67 +50,70 @@
           color:  '#B5BBC9',
         },
       ],
+      maxWidth: 0,
     }
   },
     mounted() {
-      gsap.registerPlugin(ScrollTrigger);
-      this.scrollPreference();
+      gsap.registerPlugin(ScrollTrigger)
+      this.scrollPreference()
       this.screenStatus =
       window?.innerWidth > window?.innerHeight ? 'landscape' : 'portrait'
       window.addEventListener('resize', this.registrationScrollEvent)
-      console.log(this.screenStatus);
     },
     methods: {
       registrationScrollEvent() {
       window.addEventListener('scroll', this.checkIsScreenLandscape)
       },
       checkIsScreenLandscape() {
-      const currrentScreenStatus =
-        window?.innerWidth > window?.innerHeight ? 'landscape' : 'portrait'
-      if (this.screenStatus !== currrentScreenStatus) location.reload()
-      else window.removeEventListener('scroll', this.checkIsScreenLandscape)
+        const currrentScreenStatus = window?.innerWidth > window?.innerHeight ? 'landscape' : 'portrait'
+        if (this.screenStatus !== currrentScreenStatus) location.reload()
+        else window.removeEventListener('scroll', this.checkIsScreenLandscape)
       },
       scrollPreference() {
         if (window.innerWidth > window.innerHeight) {
-          const sections = gsap.utils.toArray('.scroll_item');
-          let maxWidth = 0;
+          const sections = gsap.utils.toArray('.scroll_item')
+
           const getMaxWidth = () => {
-          maxWidth = 0;
-          sections.forEach((section) => {
-            maxWidth += section.offsetWidth;
-            });
-          };
-          getMaxWidth();
-          ScrollTrigger.addEventListener('refreshInit', getMaxWidth);
+            this.maxWidth = 0
+            sections.forEach((section) => {
+              this.maxWidth += section.offsetWidth
+            })
+          }
+
+          getMaxWidth()
+
+          ScrollTrigger.addEventListener('refreshInit', getMaxWidth)
+
           gsap.to(sections, {
-            x: () => `-${maxWidth - window.innerWidth}`,
-            ease: "none",
-            scrollTrigger: {
-              trigger: ".scroll",
-              pin: true,
-              scrub: true,
-              end: () => `+=${maxWidth}`,
-              invalidateOnRefresh: true
-            }
-          });
-            sections.forEach((sct) => {
-            let color = sct.dataset.color
-            console.log(color);
+            x: () => `-${this.maxWidth - window.innerWidth}`,
+              ease: 'none',
+              scrollTrigger: {
+                trigger: '.scroll',
+                pin: true,
+                scrub: true,
+                end: () => `+=${this.maxWidth}`,
+                invalidateOnRefresh: true
+              }
+          })
+
+          sections.forEach((sct) => {
+            const color = sct.dataset.color
+
             gsap.to(sct, {
               backgroundColor: color,
               scrollTrigger: {
                 trigger: sct,
-                start: () => 'top top-=' + (sct.offsetLeft - window.innerWidth/2) * (maxWidth / (maxWidth - window.innerWidth)),
-                end: () => '+=' + sct.offsetWidth * (maxWidth / (maxWidth - window.innerWidth)),
+                start: () => 'top top-=' + (sct.offsetLeft - window.innerWidth/2) * (this.maxWidth / (this.maxWidth - window.innerWidth)),
+                end: () => '+=' + sct.offsetWidth * (this.maxWidth / (this.maxWidth - window.innerWidth)),
                 toggleClass: {targets: sct, className: 'active'}
               }
             })
-          });
+          })
         } else {
-            const sections = gsap.utils.toArray('.scroll_item');
-            sections.forEach((sct) => {
-            let color = sct.dataset.color
-            console.log(color);
+          const sections = gsap.utils.toArray('.scroll_item')
+          sections.forEach((sct) => {
+            const color = sct.dataset.color
+
             gsap.to(sct, {
               backgroundColor: color,
               scrollTrigger: {
@@ -123,7 +123,7 @@
                 markers: true
               }
             })
-          });
+          })
         }
       }
     }
@@ -136,10 +136,10 @@
 @use '~/assets/scss/font' as f;
 @use '~/assets/scss/color' as c;
 
-
 .scroll {
   overflow: hidden;
   display: flex;
+
   @include v.mq(md) {
     display: block;
   }
@@ -156,31 +156,4 @@
   font-weight: 900;
   transition: color .3s, background-color .3s;
 }
-
 </style>
-
-<!-- <style lang="scss">
-  body {
-    overflow-x: hidden;
-  }
-  .section--large {
-    background-color: #e4002b;
-  }
-  .section--dark {
-    background-color: black;
-  }
-
-  .center-marker {
-    position: fixed;
-    width: 2px;
-    height: 100vh;
-    background: tomato;
-    top: 0;
-    left: calc(50vw - 1px);
-  }
-
-  section.active {
-    color: orange;
-  }
-</style> -->
-
