20294df418ef66640b9c4ba87b19912e48b5b007
geckou-irei <taishi.irei@geckou.net>
2022-07-21 17:20:58 +0900

    scroll.vue → index.vueに変更しました

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,32 @@
 <template>
-  <main id="frontpage" >
-    <div id="scroll" :class="$style.scroll">
-      <div id="scroll_container" 
-        :class="$style.scroll_container"
+<main id="frontpage">
+  <div :class="$style.scroll" class="scroll">
+    <section 
+      class="scroll_item"
+      v-for="item in sectionData"
+      :class="$style.section"
+      :key="item.id"
+      :data-color="item.color"
+    >
+      <CommonContainer
+        :sectionData="item"
       >
-        <section
-          class="scroll_item"
-          v-for="data in sectionData"
-          :class="$style.scroll_item"
-          :key="data.id"
-          :data-color="data.color"
-        >
-          <CommonContainer
-            :sectionData="data"
-          ></CommonContainer>
-        </section>
-      </div>
-    </div>
-  </main>
+      </CommonContainer>
+    </section>
+</div>
+
+<div class="center-marker"></div>
+</main>
 </template>
 
 <script>
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
+  import gsap from 'gsap'
+  import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-export default {
+  export default {
   data() {
     return {
-      windowStatus: "",
+      screenStatus: '',
       sectionData: [
         {
           id: 'vision',
@@ -56,122 +55,132 @@ export default {
       ],
     }
   },
-  mounted() {
-      window.addEventListener('load', this.windowWatch)  
+    mounted() {
       gsap.registerPlugin(ScrollTrigger);
-      this.$nextTick(() => {
-          this.windowWatch();
-          this.handleScroll();
-          this.windowConfirmation();
-          this.calculateWindowWidth();
-          this.backGroundColor();
-      });
-  },
-  methods: {
-    windowWatch() {
-        this.windowStatus =
-            window.innerWidth > window.innerHeight ? 'landscape' : 'portrait';
-        console.log(this.windowStatus);
+      this.scrollPreference();
+      this.screenStatus =
+      window?.innerWidth > window?.innerHeight ? 'landscape' : 'portrait'
+      window.addEventListener('resize', this.registrationScrollEvent)
+      console.log(this.screenStatus);
     },
-    windowResize() {
-        window.addEventListener('resize', this.windowResize)
-        this.handleScroll, this.calculateWindowWidth;
-    },
-    windowConfirmation() {
-        const status = window.innerWidth > window.innerHeight ? 'landscape' : 'portrait';
-        if (status === this.windowStatus) {
-            removeEventListener('scroll', this.windowConfirmation);
+    methods: {
+      registrationScrollEvent() {
+      window.addEventListener('scroll', this.checkIsScreenLandscape)
+      },
+      checkIsScreenLandscape() {
+      const currrentScreenStatus =
+        window?.innerWidth > window?.innerHeight ? 'landscape' : 'portrait'
+      if (this.screenStatus !== currrentScreenStatus) location.reload()
+      else window.removeEventListener('scroll', this.checkIsScreenLandscape)
+      },
+      scrollPreference() {
+        if (window.innerWidth > window.innerHeight) {
+          const sections = gsap.utils.toArray('.scroll_item');
+          let maxWidth = 0;
+          const getMaxWidth = () => {
+          maxWidth = 0;
+          sections.forEach((section) => {
+            maxWidth += section.offsetWidth;
+            });
+          };
+          getMaxWidth();
+          ScrollTrigger.addEventListener('refreshInit', getMaxWidth);
+          gsap.to(sections, {
+            x: () => `-${maxWidth - window.innerWidth}`,
+            ease: "none",
+            scrollTrigger: {
+              trigger: ".scroll",
+              pin: true,
+              scrub: true,
+              end: () => `+=${maxWidth}`,
+              invalidateOnRefresh: true
+            }
+          });
+            sections.forEach((sct) => {
+            let color = sct.dataset.color
+            console.log(color);
+            gsap.to(sct, {
+              backgroundColor: color,
+              scrollTrigger: {
+                trigger: sct,
+                start: () => 'top top-=' + (sct.offsetLeft - window.innerWidth/2) * (maxWidth / (maxWidth - window.innerWidth)),
+                end: () => '+=' + sct.offsetWidth * (maxWidth / (maxWidth - window.innerWidth)),
+                toggleClass: {targets: sct, className: 'active'}
+              }
+            })
+          });
+        } else {
+            const sections = gsap.utils.toArray('.scroll_item');
+            sections.forEach((sct) => {
+            let color = sct.dataset.color
+            console.log(color);
+            gsap.to(sct, {
+              backgroundColor: color,
+              scrollTrigger: {
+                trigger: sct,
+                start: 'top center',
+                end: 'bottom center',
+                markers: true
+              }
+            })
+          });
         }
-        else {
-            window.location.reload();
-        }
-    },
-    handleScroll() {
-      window.addEventListener('scroll', this.windowConfirmation);
-      window.addEventListener('resize', this.handleScroll)
-    },
-    calculateWindowWidth() {
-      window.addEventListener('resize', this.calculateWindowWidth)
-      if (window.innerWidth > window.innerHeight) {
-        const area = document.querySelector("#scroll")
-        const container = document.querySelector("#scroll_container")
-        const items = document.querySelectorAll(".scroll_item")
-        const num = items.length
-
-        gsap.set(container, { width: num * 100 + "%" })
-        gsap.set(items, { width: 100 / num + "%" })
-        gsap.to(items, {
-          xPercent: -100 * (num - 1),
-          ease: "none",
-          scrollTrigger: {
-            trigger: area,
-            start: "top top",
-            end: "+=100%",
-            scrub: true,
-            pin: true,
-            snap: {
-              // キリの良い位置へ移動させる
-              snapTo: 1 / (num - 1),
-              duration: 0.5,
-            },
-          },
-        })
       }
-    },
-    backGroundColor() {
-      // const area = document.querySelector("#scroll")
-      const els = document.querySelectorAll(".scroll_item");
-      els.forEach(el => {
-          let color = el.dataset.color // datadに格納しているカラーコードを取得
-          console.log(color);
-          gsap.to(el, {
-          duration: '1s', //５秒後かけてアニメーションさせる
-          backgroundColor: color, //dataに格納しているカラーコードを入力
-          scrollTrigger: {
-            trigger: el,
-            start: "top top",
-            end: "bottom bottom",
-            toggleActions: "play pause reverse reset",
-          }
-        })
-        console.log(el);
-      })
     }
-    
-  },
-}
+  }
 </script>
+
 
 <style lang="scss" module>
 @use '~/assets/scss/value' as v;
 @use '~/assets/scss/font' as f;
 @use '~/assets/scss/color' as c;
 
+
 .scroll {
   overflow: hidden;
-  &_container {
-    display: flex;
-    @include v.mq(md) {
-      display: block;
-    }
+  display: flex;
+  @include v.mq(md) {
+    display: block;
   }
 }
 
-.scroll_item {
-  transition: all .3s;
-  background-color: c.$white;
-}
-
-.subttl {
-  font-size: 100px;
-  color: c.$black;
-}
-
-.container {
+.section {
   display: flex;
+  width: 100vw;
   height: 100vh;
   align-items: center;
+  flex-shrink: 0;
   justify-content: center;
+  font-size: 5rem;
+  font-weight: 900;
+  transition: color .3s, background-color .3s;
 }
 
 </style>
+
+<!-- <style lang="scss">
+  body {
+    overflow-x: hidden;
+  }
+  .section--large {
+    background-color: #e4002b;
+  }
+  .section--dark {
+    background-color: black;
+  }
+
+  .center-marker {
+    position: fixed;
+    width: 2px;
+    height: 100vh;
+    background: tomato;
+    top: 0;
+    left: calc(50vw - 1px);
+  }
+
+  section.active {
+    color: orange;
+  }
+</style> -->
+
