e834553d7d2a1311f59eb03543ab2fc123743697
geckou-irei <taishi.irei@geckou.net>
2022-08-04 17:43:18 +0900

    scroll.vueの削除, plugin整理

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,131 @@
 <template>
-  <main id="frontpage">
+  <main
+    :class="$style.frontpage"
+  >
     <GradationBackground
       :class="$style.screen"
-      :sectionDatas="sectionDatas"
+      :sectionDatas="sections"
     />
     <GlobalNavigation
-      :sectionDatas="sectionDatas"
+      :sections="sections"
     />
-    <div class="scroll_wrapper">
-      <div :class="$style.scroll" class="scroll">
-        <section
-          v-for="item in sectionDatas"
-          :key="item.id"
-          :class="[$style.section, 'scroll_item']"
-          :data-color="item.color"
-        >
-          <CommonContainer
-            :sectionData="item"
-          />
-        </section>
-      </div>
+    <div
+      id="sectionsContainer"
+      :class="$style.sections_container"
+      :style="{width: `${sections.length * 100}%`}"
+    >
+      <section
+        v-for="section in sections"
+        :id="section.id"
+        :key="section.id"
+        :class="$style.section"
+      >
+        <component
+          :is="section.component"
+          :section="section"
+        />
+      </section>
     </div>
-    <div :class="$style.center_marker" />
   </main>
 </template>
 
 <script>
 import gsap from 'gsap'
-import { ScrollTrigger, } from 'gsap/ScrollTrigger'
-
+import { ScrollTrigger, ScrollToPlugin, } from 'gsap/all'
 export default {
   data() {
     return {
       screenStatus: '',
-      sectionDatas: [
+      sections    : [
         {
-          id   : 'top',
-          name : 'TOP',
-          color: 'linear-gradient(to bottom, #192c38, #0b1926 30%,  #0a1d28);',
+          id         : 'top',
+          name       : 'TOP',
+          description: 'テキストテキストテキストテキスト',
+          component  : 'SectionTop',
+          color      : 'linear-gradient(to bottom, #192c38, #0b1926 30%,  #0a1d28);',
         },
         {
-          id   : 'vision',
-          name : 'VISION',
-          color: 'linear-gradient(to bottom, #0a1d28, #192c38 30%,  #15324f);',
+          id         : 'vision',
+          name       : 'VISION',
+          description: 'テキストテキストテキストテキスト',
+          component  : 'SectionVision',
+          color      : 'linear-gradient(to bottom, #0a1d28, #192c38 30%,  #15324f);',
         },
         {
-          id   : 'service',
-          name : 'SERVICE',
-          color: 'linear-gradient(to bottom, #192c38, #15324f 34%,  #31527b);',
+          id       : 'service',
+          name     : 'SERVICE',
+          component: 'SectionService',
+          color    : 'linear-gradient(to bottom, #192c38, #15324f 34%,  #31527b);',
         },
         {
-          id   : 'information',
-          name : 'INFORMATION',
-          color: 'linear-gradient(to bottom, #31527b, #246495 66%,  #31527b);',
+          id       : 'information',
+          name     : 'INFORMATION',
+          component: 'SectionInfo',
+          color    : 'linear-gradient(to bottom, #31527b, #246495 66%,  #31527b);',
         },
         {
-          id   : 'contact',
-          name : 'CONTACT',
-          color: '#B5BBC9',
+          id       : 'contact',
+          name     : 'CONTACT',
+          component: 'SectionContact',
+          color    : '#B5BBC9',
         },
       ],
     }
   },
   mounted() {
-    gsap.registerPlugin(ScrollTrigger,)
-    this.scrollPreference()
-    this.screenStatus =
-      window?.innerWidth > window?.innerHeight ? 'landscape' : 'portrait'
+    gsap.registerPlugin(ScrollToPlugin, ScrollTrigger,)
+    this.screenStatus = window?.innerWidth > window?.innerHeight ? 'landscape' : 'portrait'
     window.addEventListener('resize', this.registrationScrollEvent,)
+    const panelsContainer = document.querySelector('#sectionsContainer',)
+    const panels = gsap.utils.toArray(`.${this.$style.section}`,)
+    const tween = gsap.to(panels, {
+      xPercent     : -100 * (panels.length - 1),
+      ease         : 'none',
+      scrollTrigger: {
+        trigger: '#sectionsContainer',
+        pin    : true,
+        start  : 'top top',
+        scrub  : true,
+        // snap: {
+        //   snapTo: 1 / (panels.length - 1),
+        //   inertia: false,
+        //   duration: {min: 0.1, max: 0.1}
+        // },
+        end    : () => `+=${panelsContainer.offsetWidth - innerWidth}`,
+      },
+    },)
+    document.querySelectorAll('.anchor',).forEach((anchor,) => {
+      anchor.addEventListener('click', (e,) => {
+        e.preventDefault()
+        const targetElem = document.querySelector(e.target.getAttribute('href',),)
+        let y = targetElem
+        if (targetElem && panelsContainer === targetElem.parentElement) {
+          const totalScroll = tween.scrollTrigger.end - tween.scrollTrigger.start
+          const totalMovement = (panels.length - 1) * targetElem.offsetWidth
+          y = Math.round(tween.scrollTrigger.start + (targetElem.offsetLeft / totalMovement) * totalScroll,)
+        }
+        gsap.to(window, {
+          scrollTo: {
+            y,
+            autoKill: false,
+          },
+          duration: 1,
+        },)
+      },)
+    },)
+
+    panels.forEach((sct,) => {
+      const target = document.querySelector(`#navi-${sct.id}`,)
+      ScrollTrigger.create({
+        trigger    : sct,
+        start      : `top top-=${sct.offsetLeft - 1}`,
+        end        : `+=${sct.offsetWidth / 2}`,
+        toggleClass: {
+          targets  : target,
+          className: 'current',
+        },
+      },)
+    },)
   },
   methods: {
     registrationScrollEvent() {
@@ -77,99 +135,33 @@ export default {
       const currrentScreenStatus = window?.innerWidth > window?.innerHeight ? 'landscape' : 'portrait'
       if (this.screenStatus !== currrentScreenStatus) { location.reload() } else { window.removeEventListener('scroll', this.checkIsScreenLandscape,) }
     },
-    scrollPreference() {
-      if (window.innerWidth > window.innerHeight) {
-        const sections = gsap.utils.toArray('.scroll_item',)
-        // const screens  = document.getElementsByClassName('screen')
-        const getMaxWidth = () => {
-          this.maxWidth = 0
-          sections.forEach((section,) => {
-            this.maxWidth += section.offsetWidth
-          },)
-        }
-
-        getMaxWidth()
-        ScrollTrigger.addEventListener('refreshInit', getMaxWidth,)
-
-        gsap.to(sections, {
-          x            : () => `-${this.maxWidth - window.innerWidth}`,
-          ease         : 'none',
-          scrollTrigger: {
-            trigger            : '.scroll',
-            pin                : true,
-            scrub              : true,
-            end                : () => `+=${this.maxWidth}`,
-            invalidateOnRefresh: true,
-          },
-        },)
-        sections.forEach((sct,) => {
-          gsap.to(sct, {
-            scrollTrigger: {
-              trigger: sct,
-              start  : () => 'top top-=' + (sct.offsetLeft - window.innerWidth / 2) * (this.maxWidth / (this.maxWidth - window.innerWidth)),
-              end    : () => '+=' + sct.offsetWidth * (this.maxWidth / (this.maxWidth - window.innerWidth)),
-            },
-          },)
-          //   gsap.to(screens,{
-          //   y: '-100vh',
-          //   scrollTrigger: {
-          //     trigger: '.scroll',
-          //     start: () => 'top top-=' + (sct.offsetLeft - window.innerWidth/2) * (this.maxWidth / (this.maxWidth - window.innerWidth)),
-          //     end: () => '+=' + sct.offsetWidth * (this.maxWidth / (this.maxWidth - window.innerWidth)),
-          //     scrub: true,
-          //   }
-          // })
-        },)
-      } else {
-        const sections = gsap.utils.toArray('.scroll_item',)
-        sections.forEach((sct,) => {
-          gsap.to(sct, {
-            scrollTrigger: {
-              trigger: sct,
-              start  : 'top center',
-              end    : 'bottom center',
-              markers: true,
-            },
-          },)
-        },)
-      }
-    },
   },
 }
 </script>
-
 <style lang="scss" module>
 @use '~/assets/scss/value' as v;
 @use '~/assets/scss/font' as f;
 @use '~/assets/scss/color' as c;
 
-.scroll {
-  display: flex;
-
-  @include v.mq(md) {
-    display: block;
-  }
+.frontpage {
+  overscroll-behavior-y: none;
+  overflow             : hidden;
+  color                : c.$white;
+}
+.sections_container {
+  height   : 100vh;
+  display  : flex;
+  flex-wrap: nowrap;
+  overflow : hidden;
 }
 
 .section {
-  display: flex;
-  width: 100vw;
-  height: 100vh;
+  position: relative;
+  display : flex;
+  width   : 100%;
+  height  : 100vh;
   align-items: center;
-  flex-shrink: 0;
   justify-content: center;
-  font-size: 5rem;
-  font-weight: 900;
-  border: solid 2px #fff;
+  overflow: hidden;
 }
-
-.center_marker {
-  position: fixed;
-  width: 2px;
-  height: 100vh;
-  background: tomato;
-  top: 0;
-  left: calc(50vw - 1px);
-}
-
 </style>
--- plugins/observe-visibility.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import VueObserveVisibility from 'vue-observe-visibility'
-
-require('intersection-observer',)
-
-Vue.use(VueObserveVisibility,)
