d7439b04be9605d482eb020d6f42aae8d2acdaa3
geckou-shogo <shogo.nojima@geckou.net>
2022-08-17 17:01:37 +0900

    lms 導入

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,31 +16,41 @@ export default {
       lang: 'ja',
     },
     meta: [
-      { charset: 'utf-8', },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1', },
-      { name: 'theme-color', content: '#288e6e', },
-      { hid: 'description', name: 'description', content: '', },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'theme-color', content: '#288e6e' },
+      { hid: 'description', name: 'description', content: '' },
     ],
     link: [
-      { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg', },
+      { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
       {
         rel  : 'apple-touch-icon',
         sizes: '180x180',
         href : '/apple-touch-icon.min.png',
       },
     ],
-    base: { href: '/', },
+    base: { href: '/' },
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/node_modules/ress/dist/ress.min.css',
+    'ress/dist/ress.min.css',
+    'locomotive-scroll/dist/locomotive-scroll.min.css',
     '~/assets/scss/base.scss',
     '~/assets/scss/nuxt-content.scss',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '@/plugins/inview.js', ssr: false, },],
+  plugins: [
+    {
+      src: '@/plugins/inview.js',
+      ssr: false,
+    },
+    {
+      src : '@/plugins/locomotiveScroll.js',
+      mode: 'client',
+    },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -69,7 +79,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    transpile: ['sal', 'gsap',],
+    transpile: ['sal', 'gsap'],
     loaders  : {
       scss: {
         implementation: Sass,
---
+++ pages/scroll.vue
@@ -0,0 +1,55 @@
+<template>
+  <section
+    id="js-scroll"
+  >
+    <div>
+      <span data-scroll>アニメーションさせたい要素</span>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      lmS: null,
+    }
+  },
+  mounted() {
+    this.lmS = new this.LocomotiveScroll({
+      el    : document.querySelector('#js-scroll'),
+      smooth: true,
+    })
+    console.log('lmS', this.lmS)
+  },
+}
+</script>
+
+<style scoped>
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 400;
+  font-size: 100px;
+  color: #2E495E;
+  letter-spacing: 1px;
+  font-size: 6em;
+}
+.green {
+  color: #00C48D;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 3em;
+  color: #2E495E;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
