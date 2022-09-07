afd7facd7db9f1bf2f70c73094ed4231f2d358cf
geckou-shogo <shogo.nojima@geckou.net>
2022-09-07 12:47:03 +0900

    色々調整

--- components/ProgressFootprints.vue
+++ components/ProgressFootprints.vue
@@ -71,5 +71,4 @@ export default {
     opacity: 1;
   }
 }
-
 </style>
--- plugins/simpleParallax.js
+++
@@ -1,3 +0,0 @@
-import Vue from 'vue'
-import simpleParallax from 'simple-parallax-js'
-Vue.prototype.$simpleParallax = simpleParallax
